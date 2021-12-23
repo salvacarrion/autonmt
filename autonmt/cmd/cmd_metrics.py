16cf7060adc8b0f9534f6e94cce85faeb60a7da5
salvacarrion <salva.carrion@outlook.com>
2021-12-23 14:54:51 +0100

    Test non-cmd functions

--- autonmt/cmd/cmd_metrics.py
+++ autonmt/cmd/cmd_metrics.py
@@ -2,9 +2,11 @@ import subprocess
 
 from autonmt import utils
 from sacrebleu.metrics import BLEU, CHRF, TER
+import bert_score
 
 
 def sacrebleu_scores(ref_file, hyp_file, output_file, metrics):
+    # Read file
     ref_lines = utils.read_file_lines(ref_file)
     hyp_lines = utils.read_file_lines(hyp_file)
 
@@ -30,6 +32,25 @@ def sacrebleu_scores(ref_file, hyp_file, output_file, metrics):
     # Save json
     utils.save_json(scores, output_file)
 
+def cmd_bertscore(ref_file, hyp_file, output_file, trg_lang, **kwargs):
+    # Read file
+    ref_lines = utils.read_file_lines(ref_file)
+    hyp_lines = utils.read_file_lines(hyp_file)
+
+    # Score
+    p, r, f1 = bert_score.score(hyp_lines, ref_lines, lang=trg_lang)
+
+    scores = [
+        {"name": "bertscore",
+         "precision": float(p.mean()),
+         "recall": float(r.mean()),
+         "f1": float(f1.mean()),
+         }
+    ]
+
+    # Save json
+    utils.save_json(scores, output_file)
+
 
 def cmd_sacrebleu(ref_file, hyp_file, output_file, metrics, conda_env_name=None):
     # The max ngram (max_ngram_order:) is default to 4 as the it was found to be the highest correlation with monolingual human judgements
@@ -48,7 +69,7 @@ def cmd_sacrebleu(ref_file, hyp_file, output_file, metrics, conda_env_name=None)
     print(f"\t- Command used: {cmd}")
 
 
-def cmd_bertscore(ref_file, hyp_file, output_file, trg_lang, conda_env_name=None):
+def _cmd_bertscore(ref_file, hyp_file, output_file, trg_lang, conda_env_name=None):
     # Run command
     env = f"conda activate {conda_env_name}" if conda_env_name else "echo \"No conda env. Using '/bin/bash'\""
     cmd = f"bert-score -r {ref_file} -c {hyp_file} --lang {trg_lang} > {output_file}"
