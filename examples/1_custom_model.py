9e989dd7520c1d5d85261b20e8208ca7f085cfea
salvacarrion <salva.carrion@outlook.com>
2022-01-01 20:33:53 +0100

    Testing

--- examples/1_custom_model.py
+++ examples/1_custom_model.py
@@ -14,12 +14,12 @@ def main():
     builder = DatasetBuilder(
         base_path="/home/salva/datasets/",
         datasets=[
-            # {"name": "multi30k", "languages": ["de-en"], "sizes": [("original", None)]},
+            {"name": "multi30k", "languages": ["de-en"], "sizes": [("original", None)]},
             # {"name": "iwlst16", "languages": ["de-en"], "sizes": [("100k", 100000)]},
             {"name": "europarl", "languages": ["de-en"], "sizes": [("100k", 100000)]},
         ],
         subword_models=["word"],
-        vocab_sizes=[1000, 2000, 4000, 8000],
+        vocab_sizes=[2000, 4000, 8000],
         merge_vocabs=False,
         force_overwrite=False,
         use_cmd=True,
@@ -36,14 +36,17 @@ def main():
     for ds in tr_datasets:
         try:
             # Instantiate vocabs and model
-            src_vocab = Vocabulary(max_tokens=150).build_from_ds(ds=ds, lang=ds.src_lang)
-            trg_vocab = Vocabulary(max_tokens=150).build_from_ds(ds=ds, lang=ds.trg_lang)
-            model = Transformer(src_vocab_size=len(src_vocab), trg_vocab_size=len(trg_vocab), padding_idx=src_vocab.pad_id)
+            src_vocab = Vocabulary(max_tokens=100).build_from_ds(ds=ds, lang=ds.src_lang)
+            trg_vocab = Vocabulary(max_tokens=100).build_from_ds(ds=ds, lang=ds.trg_lang)
+            model = Transformer(encoder_embed_dim=512, decoder_embed_dim=512,
+                                src_vocab_size=len(src_vocab), trg_vocab_size=len(trg_vocab), padding_idx=src_vocab.pad_id)
 
             # Train model
-            model = AutonmtTranslator(model=model, src_vocab=src_vocab, trg_vocab=trg_vocab, force_overwrite=False)
+            run_prefix = "model512emb"
+            model = AutonmtTranslator(run_prefix=run_prefix, model=model, src_vocab=src_vocab, trg_vocab=trg_vocab, force_overwrite=True)
             model.fit(train_ds=ds, max_epochs=75, batch_size=128, seed=1234, num_workers=16, patience=10)
             m_scores = model.predict(ts_datasets, metrics={"bleu"}, beams=[1])
+            print(m_scores)
             scores.append(m_scores)
         except Exception as e:
             print(ds)
